6097a979087c3d845023b86a55802e2a82739518
Konganwan <anwankong@gmail.com>
2018-11-11 11:24:02 +0100

    Grouped all functions in one class

--- saves.py
+++ saves.py
@@ -1,53 +1,74 @@
 # -*- coding: utf-8 -*-
 """
+    Copyright © 2018 Antoine COMBET
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this module.  If not, see <https://www.gnu.org/licenses/>.
 """
-
-from __future__ import unicode_litterals
-from codecs import open
 
 import json
 
-def create_key(save_file, key_name, key_value=None,indent=None):
-    save = {}
-    with open(save_file, 'r') as fi:
-        save = json.load(fi)
-        
-    if not save.has_key(str(key_name)):
-        save[str(key_name)] = key_value
-        
-    else:
-        raise ValueError("key already exists")
-        
-    with open(save_file, 'w') as fi:
-        json.dump(save, save_file, indent=indent)
-    
-    return 0
+class SaveManagerBase(object):
 
-def edit_key(save_file, key_name, key_value=None,indent=None):
-    save={}
-    with open(save_file, 'r') as fi:
-        save = json.load(fi)
+    def __init__(self, save_path, file_indent = None):
+        self.save_file = save_path
+        self.indent = file_indent
+
+    def create_key(self, key_name, key_value=None):
+        save = {}
+        with open(self.save_file, 'r') as fi:
+            save = json.load(fi)
+            
+        if not save.has_key(str(key_name)):
+            save[str(key_name)] = key_value
+            
+        else:
+            raise ValueError("Key already exists")
+            
+        with open(self.save_file, 'w') as fi:
+            json.dump(save, self.save_file, indent=self.indent)
         
-    if save.has_key(str(key_name)):
-        save[str(key_name)] = key_value
-        
-    else:
-        raise ValueError("key doesn't exists")
-        
-    with open(save_file, 'w') as fi:
-        json.dump(save, save_file, indent=indent)
-        
-    return 0
+        return 0
     
-def get_value(save_file, key_name):
-    save={}
-    with open(save_file, 'r') as fi:
-        save = json.load(fi)
+    def edit_key(self, key_name, key_value=None):
+        save={}
+        with open(self.save_file, 'r') as fi:
+            save = json.load(fi)
+            
+        if save.has_key(str(key_name)):
+            save[str(key_name)] = key_value
+            
+        else:
+            raise ValueError("Key doesn't exists")
+            
+        with open(self.save_file, 'w') as fi:
+            json.dump(save, fi, indent=self.indent)
+            
+        return 0
         
-    if save.has_key(str(key_name)):
-        return save[str(key_name)]
-        
-    else:
-        raise ValueError("key doesn't exists")
+    def get_value(self, key_name):
+        save={}
+        with open(self.save_file, 'r') as fi:
+            save = json.load(fi)
+            
+        if save.has_key(str(key_name)):
+            return save[str(key_name)]
+            
+        else:
+            raise ValueError("Key doesn't exists")
+            
+    def set_save_file(self, new_save):
+        self.save_file = new_save
     
+    
+        
